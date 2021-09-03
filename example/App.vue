e12d02fea63de69ec3c8bece76cfd5714b0ce610
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-03 14:16:27 +0700

    Removed plain components

--- example/App.vue
+++ example/App.vue
@@ -1,71 +1,60 @@
 <template>
-  <div>
-    <section>
-      <p>
-        Basic Components not using Veumorphic
-      </p>
-      <input type='submit'>
-      <input type='submit' :value='sample'>
-      <input type='text'>
-      <input type='text' :placeholder='sample'>
-    </section>
-    <veu-pane>
-      <p>
-        Using without sample text
-      </p>
-      <veu-button />
-      <p>
-        Using with sample text
-      </p>
-      <veu-button :text='sample' />
-      <p>
-        Rounded
-      </p>
-      <veu-button :rounded='true' />
-      <p>
-        Color Variants
-      </p>
-      <veu-button :variant='"primary"' />
-      <veu-button :variant='"secondary"' />
-      <veu-button :variant='"info"' />
-      <veu-button :variant='"success"' />
-      <veu-button :variant='"danger"' />
-      <veu-button :variant='"warning"' />
-      <p>
-        Hover Animations
-      </p>
-      <veu-button :text='"Up"' :animate='"up"' />
-      <veu-button :text='"Down"' :animate='"down"' />
-      <p>
-        Sizes
-      </p>
-      <veu-button :text='"Small"' :size='"sm"' />
-      <veu-button :text='"Regular"' :size='"md"' />
-      <veu-button :text='"Large"' :size='"lg"' />
-      <p>
-        Text-Input without placeholder
-      </p>
-      <veu-input />
-      <p>
-        Text-Input with placeholder
-      </p>
-      <veu-input :placeholder='sample' />
-      <p>
-        Text-Input of varying sizes
-      </p>
-      <veu-input :placeholder='"Small"' :size='"sm"' />
-      <veu-input :placeholder='"Regular"' :size='"md"' />
-      <veu-input :placeholder='"Large"' :size='"lg"' />
-      <p>
-        Text-Input that fits content
-      </p>
-      <veu-input :placeholder='"I fit the contents written"' :width='"fit-content"' />
-      <p>
-        Text-Input that fills available space
-      </p>
-      <veu-input :placeholder='"I fill the available space"' :width='"fill-available"' />
-    </veu-pane>
-  </div>
+  <veu-pane>
+    <p>
+      Using without sample text
+    </p>
+    <veu-button />
+    <p>
+      Using with sample text
+    </p>
+    <veu-button :text='sample' />
+    <p>
+      Rounded
+    </p>
+    <veu-button :rounded='true' />
+    <p>
+      Color Variants
+    </p>
+    <veu-button :variant='"primary"' />
+    <veu-button :variant='"secondary"' />
+    <veu-button :variant='"info"' />
+    <veu-button :variant='"success"' />
+    <veu-button :variant='"danger"' />
+    <veu-button :variant='"warning"' />
+    <p>
+      Hover Animations
+    </p>
+    <veu-button :text='"Up"' :animate='"up"' />
+    <veu-button :text='"Down"' :animate='"down"' />
+    <p>
+      Sizes
+    </p>
+    <veu-button :text='"Small"' :size='"sm"' />
+    <veu-button :text='"Regular"' :size='"md"' />
+    <veu-button :text='"Large"' :size='"lg"' />
+    <p>
+      Text-Input without placeholder
+    </p>
+    <veu-input />
+    <p>
+      Text-Input with placeholder
+    </p>
+    <veu-input :placeholder='sample' />
+    <p>
+      Text-Input of varying sizes
+    </p>
+    <veu-input :placeholder='"Small"' :size='"sm"' />
+    <veu-input :placeholder='"Regular"' :size='"md"' />
+    <veu-input :placeholder='"Large"' :size='"lg"' />
+    <p>
+      Text-Input that fits content
+    </p>
+    <veu-input :placeholder='"I fit the contents written"' :width='"fit-content"' />
+    <p>
+      Text-Input that fills available space
+    </p>
+    <veu-input :placeholder='"I fill the available space"' :width='"fill-available"' />
+  </veu-pane>
 </template>
 
 <script>
