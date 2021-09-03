e957447a6b22bee49457a43225278ade3527bb37
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-03 14:16:27 +0700

    Refactored input to button tag

--- src/components/VeuButton.vue
+++ src/components/VeuButton.vue
@@ -1,6 +1,8 @@
 <!-- Template -->
 <template>
-  <input type='submit' :value='text' />
+  <button>
+    {{ text }}
+  </button>
 </template>
 
 <!-- JavaScript -->
@@ -19,57 +21,89 @@
 
 <!-- Styles -->
 <style scoped>
-  input[type='submit']
-  {
+  button {
     margin-right: .75rem;
     margin-bottom: .75rem;
     font-size: 1rem;
     border-color: #d1d9e6;
     box-shadow: 3px 3px 6px #b8b9be,-3px -3px 6px #fff;
-    color:#31344b;
-    background-color:#e6e7ee
+    color: #31344b;
+    background-color: #e6e7ee;
   }
 
-  input[type='submit']
-  {
+  button {
     position: relative;
     transition: all .2s ease;
     letter-spacing: .025em;
   }
 
-  input[type='submit']:hover
-  {
+  button:hover {
     color:#31344b;
     background-color:#e6e7ee;
     border-color:#e6e7ee;
     box-shadow:inset 2px 2px 5px #b8b9be,inset -3px -3px 7px #fff
   }
 
-  input[type='submit']:active
-  {
+  button:active {
     box-shadow: 9px 9px 16px rgba(0, 0, 0, 0), -9px -9px 16px rgba(247, 251, 255, 0), 9px 9px 16px rgba(0, 0, 0, 0.1) inset, -9px -9px 16px rgba(247, 251, 255, 0.7) inset;
   }
 
-  input[type='submit']:disabled {
+  button:disabled {
+    pointer-events: none;
     color:#31344b;
     background-color:#e6e7ee;
     border-color:#e6e7ee
   }
-  input[type='submit']:focus {
+
+  button:focus {
     box-shadow:inset 2px 2px 5px #b8b9be,inset -3px -3px 7px #fff
   }
-  input[type='submit']:not(:disabled):not(.disabled):active,
-  input[type='submit']:not(:disabled):not(.disabled).active
-  {
+
+  button:not(:disabled):not(.disabled):active,
+  button:not(:disabled):not(.disabled).active {
     color:#31344b;
     background-color:#e6e7ee;
     border-color:#e6e7ee
   }
-  input[type='submit']:not(:disabled):not(.disabled):active:focus,
-  input[type='submit']:not(:disabled):not(.disabled).active:focus
-  {
+
+  button:not(:disabled):not(.disabled):active:focus,
+  button:not(:disabled):not(.disabled).active:focus {
     color:#31344b;
     background-color:#e6e7ee;
     border-color:#e6e7ee
   }
+
+  /**
+   * Chrome version 29 and above
+   */
+  @media screen
+    and (-webkit-min-device-pixel-ratio:0)
+    and (min-resolution:.001dpcm)
+  {
+    button {
+      -webkit-appearance: button;
+    }
+  }
+
+  /**
+   * Chrome version 22 - 28
+   */
+  @media screen
+    and (-webkit-min-device-pixel-ratio:0)
+  {
+    button {
+      -webkit-appearance: button;
+    }
+  }
+  
+
+  /**
+   * Mozilla Firefox
+   */
+  /* @supports (-moz-appearance:none) 
+  {
+    button {
+      ...
+    }
+  } */
 </style>
