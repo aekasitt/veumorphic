ef5b51ca19378bf37552b0c60019eca871a11968
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-03 14:16:26 +0700

    Removed vue3 syntax

--- example/index.js
+++ example/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
-// import { createApp, h } from 'vue';
 import App from './App.vue';
 
-// createApp({ render: () => h(App) }).mount('#app');
 new Vue({
   el: '#app',
   render: (h) => h(App)
