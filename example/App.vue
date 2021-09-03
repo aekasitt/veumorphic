b1045876292eafd1a2f63acd28de97e681d7e22a
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-03 14:16:28 +0700

    Apply flex grid badly

--- example/App.vue
+++ example/App.vue
@@ -1,64 +1,168 @@
 <template>
   <veu-pane>
-    <veu-navbar :headroom='true' :expand='"lg"'>
-      <h1>
-        Veumorphic UI Kit
-      </h1>
-    </veu-navbar>
-    <p>
-      Using without sample text
-    </p>
-    <veu-button />
-    <p>
-      Using with sample text
-    </p>
-    <veu-button :text='sample' />
-    <p>
-      Rounded
-    </p>
-    <veu-button :rounded='true' />
-    <p>
-      Color Variants
-    </p>
-    <veu-button :variant='"primary"' />
-    <veu-button :variant='"secondary"' />
-    <veu-button :variant='"info"' />
-    <veu-button :variant='"success"' />
-    <veu-button :variant='"danger"' />
-    <veu-button :variant='"warning"' />
-    <p>
-      Hover Animations
-    </p>
-    <veu-button :text='"Up"' :animate='"up"' />
-    <veu-button :text='"Down"' :animate='"down"' />
-    <p>
-      Sizes
-    </p>
-    <veu-button :text='"Small"' :size='"sm"' />
-    <veu-button :text='"Regular"' :size='"md"' />
-    <veu-button :text='"Large"' :size='"lg"' />
-    <p>
-      Text-Input without placeholder
-    </p>
-    <veu-input />
-    <p>
-      Text-Input with placeholder
-    </p>
-    <veu-input :placeholder='sample' />
-    <p>
-      Text-Input of varying sizes
-    </p>
-    <veu-input :placeholder='"Small"' :size='"sm"' />
-    <veu-input :placeholder='"Regular"' :size='"md"' />
-    <veu-input :placeholder='"Large"' :size='"lg"' />
-    <p>
-      Text-Input that fits content
-    </p>
-    <veu-input :placeholder='"I fit the contents written"' :width='"fit-content"' />
-    <p>
-      Text-Input that fills available space
-    </p>
-    <veu-input :placeholder='"I fill the available space"' :width='"fill-available"' />
+    <div class='container'>
+      <div class='row'>
+        <div class='col-20'></div>
+        <div class='col-60'>
+          <veu-navbar :headroom='true' :expand='"lg"'>
+            <div class='container'>
+              <div class='row'>
+                <div class='col-100'>
+                  <h1 class='align-center'>
+                    Veumorphic UI Kit
+                  </h1>
+                </div>
+              </div>
+              <div class='row'>
+                <div class='col-20'></div>
+                <div class='col-30'>
+                  <veu-button :text='"Buttons"' />
+                </div>
+                <div class='col-30'>
+                  <veu-button :text='"Inputs"' />
+                </div>
+                <div class='col-20'></div>
+              </div>
+            </div>
+          </veu-navbar>
+        </div>
+      </div>
+      <div class='container'>
+        <div class='row'>
+          <p>
+            Using without sample text
+          </p>
+        </div>
+        <div class='row'>
+          <veu-button />
+        </div>
+        <div class='row'>
+          <p>
+            Using with sample text
+          </p>
+        </div>
+        <div class='row'>
+          <veu-button :text='sample' />
+        </div>
+        <div class='row'>
+          <div class='col-100 align-center'>
+            <p>
+              Rounded
+            </p>
+            <veu-button :rounded='true' />
+          </div>
+        </div>
+        <div class='row'>
+            <p>
+              Color Variants
+            </p>
+        </div>
+        <div class='row'>
+          <div class='col-20'>
+            <veu-button :variant='"primary"' />
+          </div>
+          <div class='col-20'>
+            <veu-button :variant='"secondary"' />
+          </div>
+          <div class='col-20'>
+            <veu-button :variant='"info"' />
+          </div>
+          <div class='col-20'>
+            <veu-button :variant='"success"' />
+          </div>
+          <div class='col-20'>
+            <veu-button :variant='"danger"' />
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-20'>
+            <veu-button :variant='"warning"' />
+          </div>
+        </div>
+        <div class='row'>
+          <p>
+            Hover Animations
+          </p>
+        </div>
+        <div class='row'>
+          <div class='col-20'></div>
+          <div class='col-40'>
+            <veu-button :text='"Up"' :animate='"up"' />
+          </div>
+          <div class='col-40'>
+            <veu-button :text='"Down"' :animate='"down"' />
+          </div>
+        </div>
+        <div class='row'>
+          <p>
+            Sizes
+          </p>
+        </div>
+        <div class='row'>
+          <div class='col-25'>
+            <veu-button :text='"Small"' :size='"sm"' />
+          </div>
+          <div class='col-25'>
+            <veu-button :text='"Regular"' :size='"md"' />
+          </div>
+          <div class='col-25'>
+            <veu-button :text='"Large"' :size='"lg"' />
+          </div>
+        </div>
+        <div class='row'>
+          <p>
+            Text-Input without placeholder
+          </p>
+        </div>
+        <div class='row'>
+          <veu-input />
+        </div>
+        <div class='row'>
+          <p>
+            Text-Input with placeholder
+          </p>
+        </div>
+        <div class='row'>
+          <veu-input :placeholder='sample' />
+        </div>
+        <div class='row'>
+          <p>
+            Text-Input of varying sizes
+          </p>
+        </div>
+        <div class='row'>
+          <div class='col-100'>
+            <veu-input :placeholder='"Small"' :size='"sm"' />
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-100'>
+            <veu-input :placeholder='"Regular"' :size='"md"' />
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-100'>
+            <veu-input :placeholder='"Large"' :size='"lg"' />
+          </div>
+        </div>
+        <div class='row'>
+          <p>
+            Text-Input that fits content
+          </p>
+        </div>
+        <div class='row'>
+          <veu-input :placeholder='"I fit the contents written"' :width='"fit-content"' />
+        </div>
+        <div class='row'>
+          <p>
+            Text-Input that fills available space
+          </p>
+        </div>
+        <div class='row'>
+          <veu-input :placeholder='"I fill the available space"' :width='"fill-available"' />
+        </div>
+      </div>
+    </div>
   </veu-pane>
 </template>
 
@@ -79,5 +183,48 @@
   body {
     margin: 0;
     padding: 0;
+  }
+  .container {
+    border-radius: 5px;
+    padding: 20px;
+  }
+  [class*='col-'] {
+    float: left;
+    box-sizing: border-box;
+  }
+  /* Column with pre-defined width ratio: 20% */
+  .col-20 { width: 20%; }
+  /* Column with pre-defined width ratio: 25% */
+  .col-25 { width: 25%; }
+  /* Column with pre-defined width ratio: 30% */
+  .col-30 { width: 30%; }
+  /* Column with pre-defined width ratio: 40% */
+  .col-40 { width: 40%; }
+  /* Column with pre-defined width ratio: 60% */
+  .col-60 { width: 60%; }
+  /* Column with pre-defined width ratio: 70% */
+  .col-70 { width: 70%; }
+  /* Column with pre-defined width ratio: 75% */
+  .col-75 { width: 75%; }
+  /* Column with pre-defined width ratio: 80% */
+  .col-80 { width: 80%; }
+  /* Column with pre-defined width ratio: 100% */
+  .col-100 { width: 100%; }
+  /* Clear floats after the columns */
+  .row {
+    display: flex;
+    width: 100%
+  }
+  .row:after {
+    content: "";
+    display: block;
+    clear: both;
+  }
+  .hidden {
+    display: none;
+  }
+  .align-center {
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
