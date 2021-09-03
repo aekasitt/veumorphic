baf89cdd0948edd8fd21235d9096a97dd35bb5a1
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-03 14:16:26 +0700

    Updated styling for VeuButton

--- src/components/VeuButton.vue
+++ src/components/VeuButton.vue
@@ -1,16 +1,12 @@
 <!-- Template -->
 <template>
-  <div class='veumorphic'>
-    {{ text }}
-  </div>
+  <input type='submit' :value='text' />
 </template>
 
 <!-- JavaScript -->
 <script lang='javascript'>
   export default {
-    data() {
-      return {}
-    },
+    data: () => ({}),
     props: {
       text: {
         default: 'Submit',
@@ -22,29 +18,57 @@
 
 <!-- Styles -->
 <style scoped>
-  .veumorphic
+  input[type='submit']
   {
-    display: block;
-    border-radius: 4px;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    outline: 0;
-    font-weight: 600;
-    text-align: center;
-    border: 0;
-    cursor: pointer;
-    width: 100%;
-    color: #b9c2ce;
-    background-color: #b9c2ce;
-    box-shadow: 9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px rgba(255,255,255, 0.5);
+    margin-right: .75rem;
+    margin-bottom: .75rem;
+    font-size: 1rem;
+    border-color: #d1d9e6;
+    box-shadow: 3px 3px 6px #b8b9be,-3px -3px 6px #fff;
+    color:#31344b;
+    background-color:#e6e7ee
   }
-  .veumorphic:hover
+
+  input[type='submit']
   {
-    color: #e0e5ec;
-    background-color: #E0E5EC;
+    position: relative;
+    transition: all .2s ease;
+    letter-spacing: .025em;
   }
-  .veumorphic:active
+
+  input[type='submit']:hover
+  {
+    color:#31344b;
+    background-color:#e6e7ee;
+    border-color:#e6e7ee;
+    box-shadow:inset 2px 2px 5px #b8b9be,inset -3px -3px 7px #fff
+  }
+
+  input[type='submit']:active
   {
     box-shadow: 9px 9px 16px rgba(0, 0, 0, 0), -9px -9px 16px rgba(247, 251, 255, 0), 9px 9px 16px rgba(0, 0, 0, 0.1) inset, -9px -9px 16px rgba(247, 251, 255, 0.7) inset;
+  }
+
+  input[type='submit']:disabled {
+    color:#31344b;
+    background-color:#e6e7ee;
+    border-color:#e6e7ee
+  }
+  input[type='submit']:focus {
+    box-shadow:inset 2px 2px 5px #b8b9be,inset -3px -3px 7px #fff
+  }
+  input[type='submit']:not(:disabled):not(.disabled):active,
+  input[type='submit']:not(:disabled):not(.disabled).active
+  {
+    color:#31344b;
+    background-color:#e6e7ee;
+    border-color:#e6e7ee
+  }
+  input[type='submit']:not(:disabled):not(.disabled).active:focus,
+  input[type='submit']:not(:disabled):not(:disabled):active.focus
+  {
+    color:#31344b;
+    background-color:#e6e7ee;
+    border-color:#e6e7ee
   }
 </style>
