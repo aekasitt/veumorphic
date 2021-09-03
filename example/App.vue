6e443822d0752d9345ee47cad72b52d4d2e5f774
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-03 14:16:26 +0700

    Added VeuInput styling and include in example

--- example/App.vue
+++ example/App.vue
@@ -1,16 +1,32 @@
 <template>
   <section>
     <p>
-      Using default bar width and height.
+      Using without sample text
     </p>
     <veu-button />
+    <p>
+      Using with sample text
+    </p>
+    <veu-button :text='sample' />
+    <p>
+      Input without placeholder
+    </p>
+    <veu-input />
+    <p>
+      Input with placeholder
+    </p>
+    <veu-input :placeholder='sample' />
   </section>
 </template>
 
 <script>
   import VeuButton from '../src/components/VeuButton.vue';
+  import VeuInput from '../src/components/VeuInput.vue';
   export default {
-    components: { 'veu-button': VeuButton },
-    // data: () => ({  })
+    components: {
+      'veu-button': VeuButton,
+      'veu-input': VeuInput
+    },
+    data: () => ({ sample: 'Hello, World!' })
   };
 </script>
--- src/components/VeuInput.vue
+++ src/components/VeuInput.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <input :placeholder='placeholder'>
 </template>
 
 <script lang='javascript'>
   export default {
-
+    props: {
+      placeholder: {
+        default: '',
+        type: String
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  input {
+    display: block;
+    width: 100%;
+    height: calc(1.5em + 1.2rem + .0625rem);
+    padding: .6rem .75rem;
+    font-size: 1rem;
+    font-weight: 300;
+    line-height: 1.5;
+    color: #44476a;
+    background-color: #e6e7ee;
+    background-clip: padding-box;
+    border: .0625rem solid #d1d9e6;
+    border-radius: .55rem;
+    box-shadow: inset 2px 2px 5px #b8b9be,inset -3px -3px 7px #fff;
+    transition: all .3s ease-in-out
+  }
 </style>
